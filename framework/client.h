9acb8f86eb1aadc0e2587d78cc0fbf01e2b6a43b
robbey@deepsight.ai <robbey@deepsight.ai>
2020-04-29 16:49:03 +0800

    #update: framework

--- framework/client.h
+++ framework/client.h
@@ -1,19 +1,17 @@
 #ifndef REAL_APPLICATION2_CLIENTSOCKET_H_
 #define REAL_APPLICATION2_CLIENTSOCKET_H_
 
-#include "ssdp.h"
-#include "document.h"
+#include "../framework/ssdp.h"
+#include "../framework/document.h"
 #include <QTcpSocket>
 #include <QTimer>
 
-class Client : public QObject{
+class Client : public dst::configObject{
     Q_OBJECT
 public:
-    SINGLENTON(Client)
-public:
+    Client(const QJsonObject& aConfig);
     ~Client();
     void RegistOnStateChanged(std::function<void(QAbstractSocket::SocketState)> aEvent);
-    void Send(const std::string& aMessage, const QString& aID = "", std::function<void(const QJsonObject&)> aCallback = nullptr);
     bool isValid() {return valid_;}
 signals:
     void connected();
@@ -21,20 +19,14 @@ public slots:
     void ServerFound(QString aIP, QString aPort, QString aID);
     void ReceiveState(QAbstractSocket::SocketState aState);
     void ReceiveMessage();
-protected:
-    Client(QObject *parent = nullptr);
 private:
     void tryConnectServer();
     bool isValidServer(QString aIP, QString aPort);
     QTcpSocket socket_;
-    DiscoveryManager ssdp_;
+    dst::DiscoveryManager ssdp_;
     bool valid_ = false;
     QTimer search_timer_;
-    QMap<QString, std::function<void(const QJsonObject&)>> on_listened_;
+    QMap<QString, std::shared_ptr<dst::streamData>> on_listened_;
 };
-
-void SendRequest(const QJsonObject& aParam, std::function<void(const QJsonObject&)> aCallback);
-void updateGUIState(const QString& aCommand, const QJsonObject& aResponse);
-QStringList parseJsons(const QString& aContent);
 
 #endif
